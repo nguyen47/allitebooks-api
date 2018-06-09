5ddf88af6846383f3969fec22007fab07e3ff6f1
Nguyen Hoang <hoangtrankhoinguyen@gmail.com>
2018-06-09 18:21:09 +0700

    display api through route /search/:title

--- routes/index.js
+++ routes/index.js
@@ -7,34 +7,50 @@ var router = express.Router();
 var fetch = require('node-fetch');
 var cheerio = require('cheerio');
 
-// query URL
-function getURL(page, search) {
-    return url = 'http://www.allitebooks.com/page/' + page + '/?s=' + search;
-}
 
-/**
- * Load HTML from base url + search.
- * @param  {[type]} search [query to search]
- */
-function searchEbooks(page, search) {
-    return fetch(getURL(page, search))
+// url: http://www.allitebooks.com/?s=php
+var url = 'http://www.allitebooks.com/?s=';
+// page 2: http://www.allitebooks.com/page/2/?s=php
+
+function searchEbooks(query) {
+    return fetch(`${url}${query}`)
         .then(res => res.text())
         .then(body => {
+            const ebooks = [];
             const $ = cheerio.load(body);
-            $('.main-content-inner').each(function(i, element) {
-                var $element = $(element);
-                $title = $(element).find('.entry-title a');
-                console.log($title.text());
+            $('article').each(function(i, element) {
+                const $element = $(element);
+                const $title = $element.find('.entry-title a');
+                const $image = $element.find('.attachment-post-thumbnail');
+                const $description = $element.find('.entry-summary');
+                const authors = [];
+                $(element).find('.entry-author a').each(function(i, element) {
+                    author = $(element).text();
+                    authors.push(author);
+                });
+                const ebook = {
+                    image: $image.attr('src'),
+                    title: $title.text(),
+                    description: $description.text(),
+                    authors: authors,
+                }
+                ebooks.push(ebook);
             });
+            return ebooks;
         });
 }
 
 router.get('/', function(req, res, next) {
-    res.send('API RUNNING');
+    res.json({
+        message: "Running ... "
+    });
 });
 
-router.get('/search/:title', function(req, res, next) {
-    res.json(searchEbooks(1, req.params.title));
+router.get('/search/:title', function(req, res, next){
+    searchEbooks(req.params.title)
+    .then(ebooks => {
+        res.json(ebooks);
+    });
 });
 
 module.exports = router;
